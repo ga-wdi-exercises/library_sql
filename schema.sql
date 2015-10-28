c7d327c1af93845e699fa91dad7c8fe4e1e1e5b4
Adam Bray <adam.bray@yale.edu>
2015-10-28 10:07:21 -0400

    corrected schema

--- schema.sql
+++ schema.sql
@@ -2,14 +2,14 @@ DROP TABLE IF EXISTS books;
 DROP TABLE IF EXISTS authors;
 
 CREATE TABLE authors (
-  id INTEGER PRIMARY KEY,
+  id INTEGER SERIAL PRIMARY KEY,
   name VARCHAR(255),
   nationality VARCHAR(255),
   birth_year INTEGER
 );
 
 CREATE TABLE books (
-  id INTEGER PRIMARY KEY,
+  id INTEGER SERIAL PRIMARY KEY,
   title VARCHAR(255),
   publication_date INTEGER,
   author_id INTEGER
