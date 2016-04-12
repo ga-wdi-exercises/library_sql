12108956cb8729df54e7a9ad6a586471ff9ae7f0
Adam Bray <adam.bray@gmail.com>
2016-04-12 09:22:54 -0400

    Update basic_queries.sql

--- basic_queries.sql
+++ basic_queries.sql
@@ -28,3 +28,25 @@ SELECT publication_date FROM books WHERE title='A Dance with Dragons';
 
 -- Get all books which have `the` somewhere in their title (hint, look up LIKE/ILIKE)
 SELECT * FROM books where title ILIKE '%the%';
+
+-- Add yourself as an author
+INSERT INTO authors (name, nationality, birth_year) VALUES ('Adam', 'USA', 1985);
+-- Note we omit the ID because the authors table has a SERIAL PRIMARY KEY constraint
+-- so the DB adds a new ID for us automatically.
+
+-- Add two books that you'd like to write (you can hard-code your id as the author id)
+-- Check your author ID: SELECT * FROM authors WHERE name='Adam';
+-- Assume we get, say: 12
+INSERT INTO books (title, publication_date, author_id) VALUES ('Programmin Aint Easy', 2015, 12);
+INSERT INTO books (title, publication_date, author_id) VALUES ('Fly Fishing for Programmers', 2016, 12);
+
+-- Update one of your books to have a new title
+UPDATE authors SET title = 'Volleyball for Programmers' WHERE title ='Fly Fishing for Programmers';
+
+-- Delete your books
+DELETE FROM authors where title = 'Volleyball for Programmers' OR title = 'Programmin Aint Easy';
+
+-- Delete your author entry
+DELETE FROM authors where name = 'Adam';
+-- OR if we found the ID earlier, use that to find and delete instead.
+DELETE FROM authors where ID = 12;
